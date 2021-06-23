64f749cf75f89c87f97eea459498d9cd4d0b25ee
somantalha <somantalha98@gmail.com>
2021-06-23 22:01:02 +0500

    Update GenericService.js

--- src/services/GenericService.js
+++ src/services/GenericService.js
@@ -56,8 +56,8 @@
 
 import axios from "axios";
 axios.defaults.headers.common["x-auth-token"] = localStorage.getItem("token");
-// axios.defaults.baseURL = "http://localhost:4000/api/";
-axios.defaults.baseURL = "https://restfulapi-backend.herokuapp.com/api/";
+axios.defaults.baseURL = "http://localhost:4000/api/";
+// axios.defaults.baseURL = "https://restfulapi-backend.herokuapp.com/api/";
 
 class GenericService {
   constructor() {}
